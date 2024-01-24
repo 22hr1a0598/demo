2030bc7b85d53c468c02bbc06e97cd125edaf907
22hr1a0598 <154883815+22hr1a0598@users.noreply.github.com>
2024-01-24 19:55:26 +0530

    Add files via upload

---
+++ ExceptionEX.java
@@ -0,0 +1,12 @@
+class EX{
+public static void main(String args[]){
+//int a=/;
+//Aritheematic exception
+//String s=null;
+//System.out.println(s.length());//nullpointerexception
+/*String s="abc";
+int i=Integer.parseInt(s);//numberformatException
+*/
+int a[]=new int[5];
+a[10]=50;//ArrayIndexOutOfBoundException
+}}
---
+++ Vowels.java
@@ -0,0 +1,9 @@
+class Vowels{
+public static void main(String[] args) {
+String str = "Today is last day";
+str = str.toLowerCase(); 
+for (int i = 0; i < str.length(); i++) {
+char ch = str.charAt(i);
+if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+System.out.print(ch + " ");}
+}}}
